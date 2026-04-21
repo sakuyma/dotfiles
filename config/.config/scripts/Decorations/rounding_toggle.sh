bb97e4a40a42214dbf0d100489e31e879047c0ce
sakuyma <yerkin.timofey@protonmail.com>
2026-04-21 16:55:07 +0300

    CHORE: moved to smaller roundings

--- config/.config/scripts/Decorations/rounding_toggle.sh
+++ config/.config/scripts/Decorations/rounding_toggle.sh
@@ -3,8 +3,8 @@
 LOCK_FILE="/tmp/rounding.lock"
 
 if [ -f "$LOCK_FILE" ]; then
-    hyprctl keyword decoration:rounding 25 
-    hyprctl keyword decoration:rounding_power 10
+    hyprctl keyword decoration:rounding 12 
+    hyprctl keyword decoration:rounding_power 3 
     rm -f "$LOCK_FILE"
 else
     hyprctl keyword decoration:rounding 0 
